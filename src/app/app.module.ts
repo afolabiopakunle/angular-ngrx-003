5f4fc411750b3cd0fa5f888706f88e38c475a5f2
afolabiopakunle <opakunleafolabi@gmail.com>
2023-05-23 14:58:39 +0100

    configs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialsModule } from './angular-materials/angular-materials.module';
 import { PostsModule } from './posts/posts.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment.prod';
+
 
 @NgModule({
   imports: [
@@ -13,6 +17,12 @@ import { PostsModule } from './posts/posts.module';
     BrowserAnimationsModule,
     AngularMaterialsModule,
     PostsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+      autoPause: true,
+    })
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
